9877b72abb57293acd8a340eac5a863aef41419d
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-23 02:02:16 +0000

    Added def valid word section this will randomly pick a random word from the list in the game

--- run.py
+++ run.py
@@ -1,2 +1,18 @@
 import random
+import string
+
 from word import words
+
+def get_valid_word (words):
+    word =random.choice(words)
+    # this will randomly pick a random word from the list 
+    while '_' in words or ' ' in words:
+
+        return words 
+
+def thehangmangame():
+    word = get_valid_word(words)
+    word_letters = set(word) 
+    # 
+    Alphabet = set(string.ascii_uppercase)
+
--- word.py
+++ word.py
@@ -1,3 +1,4 @@
 words = [Adult , Aeroplane , Air , Aircraft Carrier , Airforce , Airport , Album , Alphabet , Apple , Desk , Diamond , Dress
-,Drill ,Drink, Film ,Finger ,Fire , Milk , Store , Bottle , Outout, Results]
+,Drill ,Drink, Film ,Finger ,Fire , Milk , Store , Bottle , Out-put, Results]
 
+# list of words are from this site "http://members.optusnet.com.au/~charles57/Creative/Techniques/random_words.htm" 
