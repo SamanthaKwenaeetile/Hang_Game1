c8bf9691a1e881ff93157630d18f6da4090635f4
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-23 01:15:18 +0000

    Add random words into word.py

---
+++ word.py
@@ -0,0 +1,3 @@
+words = [Adult , Aeroplane , Air , Aircraft Carrier , Airforce , Airport , Album , Alphabet , Apple , Desk , Diamond , Dress
+,Drill ,Drink, Film ,Finger ,Fire , Milk , Store , Bottle , Outout, Results]
+
