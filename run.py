a5f8e673e4089c441a533b13ad22a8c833060c97
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-23 02:20:39 +0000

    Add error in code from word.py file when trying to run game

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-from word import words
+from words import words
 
 def get_valid_word (words):
     word =random.choice(words)
@@ -17,4 +17,20 @@ def thehangmangame():
     Alphabet = set(string.ascii_lowercase)
     used_letters = set() 
      #  this will keep the words that the user has gussed.
+
+# the user input 
+user_letter = input('Guess a letter:').ascii_lowercase()
+if user_letter in Alphabet - used_letters:
+    used_letters.add(used_letter)
+    if used_letter in word_letters:
+        word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You used the same letter before try again')
+    
+    else:
+        print('invalid letter enterd please try again')
+        
+user_input = input ('enter a letter here')
+print(user_input)
 
